5b033674fb687668927e73421409d3cada047f0e
Daniel-Slavnyi <dslavnyj00@gmail.com>
2022-11-13 22:25:26 +0300

    third commit

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,30 @@
+import Player from '@vimeo/player';
+import lodash from "lodash.throttle";
 
+const iframeEl = document.querySelector('iframe');
+const player = new Player(iframeEl);
+const TIME = 'currentTime';
+
+const lodashOn = lodash(onPlayerTimeUpdate, 1000);
+
+player.on('timeupdate', lodashOn);
+function onPlayerTimeUpdate(e) {
+    localStorage.setItem(TIME, e.seconds);
+}
+
+const currentTime = Number(localStorage.getItem(TIME));
+console.log(currentTime);
+
+player.setCurrentTime(currentTime).then(function() {
+    // seconds = the actual time that the player seeked to
+}).catch(function(error) {
+    switch (error.name) {
+        case 'RangeError':
+            // the time was less than 0 or greater than the video’s duration
+            break;
+
+        default:
+            // some other error occurred
+            break;
+    }
+});
